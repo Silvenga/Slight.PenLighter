80f602f7363a2c2cb557613f99fea82928be9210
Arthur Zaczek <zaczek@technikum-wien.at>
2015-07-09 08:11:10 +0200

    reformat

--- Hooks/HookManager.cs
+++ Hooks/HookManager.cs
@@ -1,13 +1,12 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-
 using SlightPenLighter.Models;
 
-namespace SlightPenLighter.Hooks {
-
-    public static class HookManager {
-
+namespace SlightPenLighter.Hooks
+{
+    public static class HookManager
+    {
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         private static extern int CallNextHookEx(int idHook, int nCode, int wParam, IntPtr lParam);
 
@@ -25,13 +24,16 @@ namespace SlightPenLighter.Hooks {
 
         private static event MousePointChange MouseMoveBackend;
 
-        public static event MousePointChange MouseMove {
-            add {
+        public static event MousePointChange MouseMove
+        {
+            add
+            {
                 EnsureSubscribedToGlobalMouseEvents();
                 MouseMoveBackend += value;
             }
 
-            remove {
+            remove
+            {
                 MouseMoveBackend -= value;
                 TryUnsubscribeFromGlobalMouseEvents();
             }
@@ -45,68 +47,66 @@ namespace SlightPenLighter.Hooks {
         private static int _oldX;
         private static int _oldY;
 
-        private static int MouseHookProc(int nCode, int wParam, IntPtr lParam) {
+        private static int MouseHookProc(int nCode, int wParam, IntPtr lParam)
+        {
 
-            if(nCode >= 0) {
+            if (nCode >= 0)
+            {
+                var mouseHook = (MouseHook)Marshal.PtrToStructure(lParam, typeof(MouseHook));
 
-                var mouseHook = (MouseHook) Marshal.PtrToStructure(lParam, typeof(MouseHook));
+                if (MouseMoveBackend != null)
+                {
+                    if (_oldX != mouseHook.PhysicalPoint.X || _oldY != mouseHook.PhysicalPoint.Y)
+                    {
+                        _oldX = mouseHook.PhysicalPoint.X;
+                        _oldY = mouseHook.PhysicalPoint.Y;
 
-                if((MouseMoveBackend != null)
-                   && (_oldX != mouseHook.PhysicalPoint.X || _oldY != mouseHook.PhysicalPoint.Y)) {
-
-                    _oldX = mouseHook.PhysicalPoint.X;
-                    _oldY = mouseHook.PhysicalPoint.Y;
-
-                    MouseMoveBackend.Invoke(mouseHook.PhysicalPoint);
+                        MouseMoveBackend.Invoke(mouseHook.PhysicalPoint);
+                    }
                 }
             }
 
             return CallNextHookEx(_mouseHookHandle, nCode, wParam, lParam);
         }
 
-        private static void EnsureSubscribedToGlobalMouseEvents() {
-
-            if(_mouseHookHandle == 0) {
-
+        private static void EnsureSubscribedToGlobalMouseEvents()
+        {
+            if (_mouseHookHandle == 0)
+            {
                 _mouseDelegate = MouseHookProc;
-
                 _mouseHookHandle = SetWindowsHookEx(WhMouseLl, _mouseDelegate, IntPtr.Zero, 0);
 
-                if(_mouseHookHandle == 0) {
-
+                if (_mouseHookHandle == 0)
+                {
                     var status = Marshal.GetLastWin32Error();
-
                     throw new Win32Exception(status);
                 }
             }
         }
 
-        private static void TryUnsubscribeFromGlobalMouseEvents() {
-
-            if(MouseMoveBackend == null) {
-
+        private static void TryUnsubscribeFromGlobalMouseEvents()
+        {
+            if (MouseMoveBackend == null)
+            {
                 ForceUnsunscribeFromGlobalMouseEvents();
             }
         }
 
-        private static void ForceUnsunscribeFromGlobalMouseEvents() {
-
-            if(_mouseHookHandle != 0) {
-
+        private static void ForceUnsunscribeFromGlobalMouseEvents()
+        {
+            if (_mouseHookHandle != 0)
+            {
                 var result = UnhookWindowsHookEx(_mouseHookHandle);
 
                 _mouseHookHandle = 0;
                 _mouseDelegate = null;
 
-                if(result == 0) {
-
+                if (result == 0)
+                {
                     var status = Marshal.GetLastWin32Error();
-
                     throw new Win32Exception(status);
                 }
             }
         }
-
     }
-
 }
--- Hooks/MouseTracker.cs
+++ Hooks/MouseTracker.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-
 using SlightPenLighter.Models;
 
-namespace SlightPenLighter.Hooks {
-
-    public class MouseTracker {
-
-        public IntPtr WindowPointer {
+namespace SlightPenLighter.Hooks
+{
+    public class MouseTracker
+    {
+        public IntPtr WindowPointer
+        {
             get;
             set;
         }
 
-        public MouseTracker(IntPtr window) {
-
+        public MouseTracker(IntPtr window)
+        {
             WindowPointer = window;
             HookManager.MouseMove += HookManagerOnMouseMove;
         }
 
-        private void HookManagerOnMouseMove(PhysicalPoint next) {
-
+        private void HookManagerOnMouseMove(PhysicalPoint next)
+        {
             DwmHelper.MoveWindow(WindowPointer, next.X, next.Y);
         }
-
     }
-
 }
