438feb200431d3fb7fce9a0fbc2c3d84a8d7d470
Mark Lopez <m@silvenga.com>
2020-10-23 17:56:40 -0500

    Per profiling, enabled low latency mode.

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿namespace SlightPenLighter
+﻿using System.Runtime;
+using System.Windows;
+
+namespace SlightPenLighter
 {
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+            base.OnStartup(e);
+        }
     }
 }
